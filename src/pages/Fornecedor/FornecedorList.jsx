a930c707a4a58e2ae7037eb2b1d0c121c5f1a8d3
gessica <gessica.melo@live.com>
2024-08-12 19:57:41 -0300

    Aula 06 - Andamento

--- src/pages/Fornecedor/FornecedorList.jsx
+++ src/pages/Fornecedor/FornecedorList.jsx
@@ -8,131 +8,139 @@ const FornecedorList = () => {
 
     // Declara uma variável de estado chamada "fornecedores" e uma função "setFornecedores" para atualizá-la.
     // Inicialmente, "fornecedores" é um array vazio.
-    const [fornecedores, setFornecedores] = useState([])
-    const [fornecedorSelecionado, setFornecedorSelecionado] = useState(null)
-    const [modalAberto, setModalAberto] = useState(false)
-    const [modalSucessoAberto, setModalSucessoAberto] = useState(false)
+    const [fornecedores, setFornecedores] = useState([]);
 
+    // Estado para armazenar o fornecedor atualmente selecionado.
+    const [fornecedorSelecionado, setFornecedorSelecionado] = useState(null);
 
+    // Estado para controlar a visibilidade do modal de confirmação de exclusão.
+    const [modalAberto, setModalAberto] = useState(false);
+
+    // Estado para controlar a visibilidade do modal de sucesso após exclusão.
+    const [modalSucessoAberto, setModalSucessoAberto] = useState(false);
 
     // useEffect é usado para executar código quando o componente é montado na tela.
     // Aqui, estamos buscando os dados dos fornecedores.
-    useEffect (() => {
+    useEffect(() => {
         // Função para buscar os fornecedores da API
         const buscarFornecedores = () => {
             // Faz uma requisição HTTP GET para a URL "/fornecedores" usando axios.
             axios.get('/fornecedores')
                 // Quando a requisição é bem-sucedida, atualiza o estado "fornecedores" com os dados recebidos.
-            .then(response => {
-                setFornecedores(response.data)
-            })
-            .catch(error => {
-                // Se ocorrer um erro durante a requisição, exibe uma mensagem de erro no console.
-                console.error("Ocorreu um erro", error)
-            })
-        }
+                .then(response => {
+                    setFornecedores(response.data);
+                })
+                .catch(error => {
+                    // Se ocorrer um erro durante a requisição, exibe uma mensagem de erro no console.
+                    console.error("Ocorreu um erro", error);
+                });
+        };
         // Chama a função para buscar os fornecedores quando o componente é montado.
-        buscarFornecedores()
-    }, []) // O array vazio [] garante que o useEffect seja executado apenas uma vez, após a montagem do componente.
+        buscarFornecedores();
+    }, []); // O array vazio [] garante que o useEffect seja executado apenas uma vez, após a montagem do componente.
 
+    // Função para abrir o modal de confirmação de exclusão.
+    const abrirModal = (fornecedor) => {
+        setFornecedorSelecionado(fornecedor); // Define o fornecedor selecionado.
+        setModalAberto(true); // Abre o modal.
+    };
 
-    const abrirModal = (fornecedor) =>{
-        setFornecedorSelecionado(fornecedor)
-        setModalAberto(true)
-    }
-
+    // Função para fechar o modal de confirmação de exclusão.
     const fecharModal = () => {
-        setModalAberto(false)
-        setFornecedorSelecionado(null)
-    }
+        setModalAberto(false); // Fecha o modal.
+        setFornecedorSelecionado(null); // Reseta o fornecedor selecionado.
+    };
 
+    // Função para abrir o modal de sucesso.
     const abrirModalSucesso = () => {
-        setModalSucessoAberto(true)
-        setTimeout (() => setModalSucessoAberto(false), 2000)
-    }
+        setModalSucessoAberto(true); // Abre o modal de sucesso.
+        setTimeout(() => setModalSucessoAberto(false), 2000); // Fecha automaticamente após 2 segundos.
+    };
 
+    // Função para remover o fornecedor selecionado.
     const removerFornecedor = () => {
-        axios.delete(`/fornecedores/${fornecedorSelecionado.id}`)
-        .then(() => {
-            setFornecedores(prevFornecedores => prevFornecedores.filter(fornecedor => fornecedor.id !== fornecedorSelecionado.id))
-            fecharModal()
-            abrirModalSucesso()
-        })
-    }
+        axios.delete(`/fornecedores/${fornecedorSelecionado.id}`) // Faz a requisição DELETE para remover o fornecedor.
+            .then(() => {
+                // Atualiza o estado "fornecedores" removendo o fornecedor excluído.
+                setFornecedores(prevFornecedores => prevFornecedores.filter(fornecedor => fornecedor.id !== fornecedorSelecionado.id));
+                fecharModal(); // Fecha o modal de confirmação.
+                abrirModalSucesso(); // Abre o modal de sucesso.
+            });
+    };
 
-  return (
-    <div className="container mt-5">
-        <h2 className="mb-4"> Lista de Fornecedores</h2>
-        <Link to="/add-fornecedores" className="btn btn-primary mb-2">
-            <FaPlus className="icon" /> Adicionar Fornecedor
-        </Link>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Nome:</th>
-                    <th>CNPJ:</th>
-                    <th>Email:</th>
-                    <th>Ações:</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    fornecedores.map(fornecedor => (
+    return (
+        <div className="container mt-5">
+            <h2 className="mb-4">Lista de Fornecedores</h2>
+
+            {/* Link para adicionar um novo fornecedor */}
+            <Link to="/add-fornecedores" className="btn btn-primary mb-2">
+                <FaPlus className="icon" /> Adicionar Fornecedor
+            </Link>
+
+            {/* Tabela de fornecedores */}
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Nome:</th>
+                        <th>CNPJ:</th>
+                        <th>Email:</th>
+                        <th>Ações:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* Mapeia os fornecedores para criar linhas na tabela */}
+                    {fornecedores.map(fornecedor => (
                         <tr key={fornecedor.id}>
                             <td>{fornecedor.nome}</td>
                             <td>{fornecedor.cnpj}</td>
                             <td>{fornecedor.email}</td>
                             <td>
-                                <Link to={`/edit-fornecedores/${fornecedor.id}`}
-                                 className="btn-sm btn-warning">
-                                    <FaEdit className="icon icon-btn"/> Editar                                
+                                {/* Link para editar o fornecedor */}
+                                <Link to={`/edit-fornecedores/${fornecedor.id}`} className="btn-sm btn-warning">
+                                    <FaEdit className="icon icon-btn" /> Editar
                                 </Link>
+                                {/* Botão para abrir o modal de exclusão */}
                                 <button onClick={() => abrirModal(fornecedor)} className='btn btn-sm btn-danger'>
-                                    <FaTrash className="icon icon-btn"/> Excluir
+                                    <FaTrash className="icon icon-btn" /> Excluir
                                 </button>
                             </td>
-
                         </tr>
-                    ))
-                }
-            </tbody>
+                    ))}
+                </tbody>
+            </table>
 
-        </table>
-        <Modal
-            isOpen={modalAberto}
-            onRequestClose={fecharModal}
-            className="modal"
-            overlayClassName="overlay"
-        >
-            <div className='modalContent'>
-                <FaExclamationTriangle className="icon" />
-                <h2>Confirmar Exclusão</h2>
-                <p>Tem certeza que deseja excluir o fornecedor 
-                {fornecedorSelecionado && fornecedorSelecionado.nome}?                
-                </p>
-                <div className="modalButtons">
-                    <button onClick={fecharModal} className="btn btn-secondary">Cancelar</button>
-                    <button onClick={removerFornecedor} className="btn btn-danger">Excluir</button>
-                </div>   
-            </div>
-            
-        </Modal>
+            {/* Modal de confirmação de exclusão */}
+            <Modal
+                isOpen={modalAberto}
+                onRequestClose={fecharModal}
+                className="modal"
+                overlayClassName="overlay"
+            >
+                <div className='modalContent'>
+                    <FaExclamationTriangle className="icon" />
+                    <h2>Confirmar Exclusão</h2>
+                    <p>Tem certeza que deseja excluir o fornecedor {fornecedorSelecionado && fornecedorSelecionado.nome}?</p>
+                    <div className="modalButtons">
+                        <button onClick={fecharModal} className="btn btn-secondary">Cancelar</button>
+                        <button onClick={removerFornecedor} className="btn btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </Modal>
 
-        <Modal
-            isOpen={modalSucessoAberto}
-            onRequestClose={() => setModalSucessoAberto(false)}
-            className="modal"
-            overlayClassName="overlay"
-        >
-            <div className="modalContent">
-                <FaCheckCircle className="icon successIcon"/>
-                <h2>Fornecedor excluído com sucesso!</h2>
-            </div>
-        </Modal>
-
-
-    </div>
-  )
-}
+            {/* Modal de sucesso após exclusão */}
+            <Modal
+                isOpen={modalSucessoAberto}
+                onRequestClose={() => setModalSucessoAberto(false)}
+                className="modal"
+                overlayClassName="overlay"
+            >
+                <div className="modalContent">
+                    <FaCheckCircle className="icon successIcon" />
+                    <h2>Fornecedor excluído com sucesso!</h2>
+                </div>
+            </Modal>
+        </div>
+    );
+};
 
 export default FornecedorList
